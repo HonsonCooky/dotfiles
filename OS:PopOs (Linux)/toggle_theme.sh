1643d1748e4c4b7fe21ecd0da0a3d860d6b50f79
Harrison Cook <HonsonCooky@gmail.com>
2023-12-28 19:47:10 +1300

    update toggle theme

--- OS:PopOs (Linux)/toggle_theme.sh
+++ OS:PopOs (Linux)/toggle_theme.sh
@@ -7,17 +7,16 @@ current_theme=$(gsettings get org.gnome.desktop.interface gtk-theme)
 light_theme="Catppuccin-Latte"
 dark_theme="Catppuccin-Mocha-dark"
 
-# Check the current theme and switch
-if [ $1 == "dark" ]; then
-	gsettings set org.gnome.shell.ubuntu color-scheme prefer-dark
-	gsettings set org.gnome.desktop.interface color-scheme prefer-dark
-	gsettings set org.gnome.desktop.interface gtk-theme "$dark_theme"
-	gsettings set org.gnome.shell.extensions.user-theme name "$dark_theme"
-	gsettings set org.gnome.Terminal.ProfilesList default "95894cfd-82f7-430d-af6e-84d168bc34f5"
-else
+if [[ $1 == 'light' || (-z $1 && $current_theme = *'dark'*) ]]; then
 	gsettings set org.gnome.shell.ubuntu color-scheme prefer-light
 	gsettings set org.gnome.desktop.interface color-scheme prefer-light
 	gsettings set org.gnome.desktop.interface gtk-theme "$light_theme"
 	gsettings set org.gnome.shell.extensions.user-theme name "$light_theme"
 	gsettings set org.gnome.Terminal.ProfilesList default "de8a9081-8352-4ce4-9519-5de655ad9361"
+else
+	gsettings set org.gnome.shell.ubuntu color-scheme prefer-dark
+	gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+	gsettings set org.gnome.desktop.interface gtk-theme "$dark_theme"
+	gsettings set org.gnome.shell.extensions.user-theme name "$dark_theme"
+	gsettings set org.gnome.Terminal.ProfilesList default "95894cfd-82f7-430d-af6e-84d168bc34f5"
 fi
